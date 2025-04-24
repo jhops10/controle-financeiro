15503dbae508ec3dd0ea4f8602b8252555b83777
João Paulo <joaowais@gmail.com>
2025-04-24 15:55:57 -0300

    test(SummaryService): adiciona testes unitários para método getSummary

---
+++ src/test/java/com/jhops10/personal_finance/common/SummaryConstants.java
@@ -0,0 +1,14 @@
+package com.jhops10.personal_finance.common;
+
+import java.math.BigDecimal;
+
+public class SummaryConstants {
+
+    public static final Long USER_ID = 1L;
+    public static final int MONTH = 4;
+    public static final int YEAR = 2025;
+
+    public static final BigDecimal EXPECTED_INCOME = new BigDecimal("5000.00");
+    public static final BigDecimal EXPECTED_EXPENSE = new BigDecimal("3000.00");
+    public static final BigDecimal EXPECTED_BALANCE = new BigDecimal("2000.00");
+}
