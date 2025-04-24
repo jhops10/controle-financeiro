9c0d5ea22a1f713d190c6dc2841f9e2443291a1b
João Paulo <joaowais@gmail.com>
2025-04-24 15:35:40 -0300

    feat(ExpenseRepository, IncomeRepository): adiciona método getTotalIncomeByMonthAndYear e getTotalExpenseByMonthAndYear

--- src/main/java/com/jhops10/personal_finance/transactions/expense/ExpenseRepository.java
+++ src/main/java/com/jhops10/personal_finance/transactions/expense/ExpenseRepository.java
@@ -1,12 +1,18 @@
 package com.jhops10.personal_finance.transactions.expense;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     List<Expense> findByUserId(Long userId);
+
+    @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e WHERE e.user.id = :userId AND MONTH(e.date) = :month AND YEAR(e.date) = :year")
+    BigDecimal getTotalExpenseByMonthAndYear(Long userId, int month, int year);
+
 }
