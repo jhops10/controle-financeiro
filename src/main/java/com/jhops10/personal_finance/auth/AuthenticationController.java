bf6bfa404205266f4c2d1e47ec1734e3b97f67a1
João Paulo <joaowais@gmail.com>
2025-04-25 16:13:47 -0300

    (auth): adiciona dto, service e controller com método login

---
+++ src/main/java/com/jhops10/personal_finance/auth/AuthenticationController.java
@@ -0,0 +1,21 @@
+package com.jhops10.personal_finance.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDTO> login(@RequestBody AuthenticationRequestDTO request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
